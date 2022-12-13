4823ad4eb33745f3cd28b06d0b2710e0ecd99eaf
Arjen <81611104+arjenou@users.noreply.github.com>
2022-12-13 23:06:22 +0900

    Update mediapipe_test.py

--- mediapipe_test.py
+++ mediapipe_test.py
@@ -103,7 +103,7 @@ while True:
             img = cv2.circle(img,(cx,cy), radius, (94,218,121), -1)
         else: 
             img = cv2.circle(img,(cx,cy), radius, (0,255,0), -1)
-        print("検知成功画像数：",i)
+       print("検知成功画像数：",i)
     else:
       l=l+1
       print("no kindpoint",imgpath)
